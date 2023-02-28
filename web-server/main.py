ca1003a01a951da4b3f866982e060bbdffb211be
iPoe <leonardosawi12@gmail.com>
2023-02-27 20:34:07 -0500

    Use fast api and serve html

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,40 @@
 import store
 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return """
+    <html>
+        <head>
+            <title> Crane AI</title>
+        </head>
+        <body>
+            <h1>Hola, gracias por querer contactarnos</h1>
+        <body>
+    </html>
+    """
+
+@app.get('/landing', response_class=HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>
+            <title> Crane AI</title>
+        </head>
+        <body>
+            <h1>Servicios a tu medida</h1>
+        <body>
+    </html>
+    """
+
 def run():
     store.get_categories()
 
